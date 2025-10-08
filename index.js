2c1a781f8a6d0bdf37d3b6111b2bc24ba4f5a618
Toem Sopheara <sopheara.toem@gmail.com>
2025-10-08 16:17:34 +0700

    Change Hello

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
-    res.send('Hello, Docker');
+    res.send('Hello, I am running form github');
 });
 
 app.listen(PORT, ()=> {
